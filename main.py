e9f691ff639c04be51893c1031535a83ec2eaa25
Bohdan <bogdannew@ukr.net>
2025-08-18 13:51:27 +0300

    Add basic movie endpoints

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from routers import users, movies
+from database import engine
+from models import Base
+
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(users.router)
+app.include_router(movies.router)
